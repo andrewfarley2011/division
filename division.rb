791a4149072b6e8b6588646db015ea0ea0e2a6ee
andrewfarley2011 <andrewfarley@yahoo.com>
2017-06-27 11:20:17 -0400

    Division C

--- division.rb
+++ division.rb
@@ -1,14 +1,20 @@
-def denominator
-	denominator != 0
+
+# def denominator
+# 	denominator != 0
+# end
+
+def quotient(numerator,denominator)
+	if denominator == 0
+		puts "Does Not Exist"
+	else 
+		puts "#{numerator} / #{denominator} = #{numerator / denominator}"
+	end
 end
 
 puts "Numerator: " 
-numerator = gets.to_i 
+numerator = gets.to_i
  
 puts "Denominator: " 
 denominator = gets.to_i 
  
-quotient = numerator / denominator 
- 
-puts "#{numerator} / #{denominator} = #{quotient}" 
-	
+quotient(numerator,denominator) 
