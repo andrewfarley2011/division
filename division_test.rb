8fd99a58d30f661d29158458e546b6cf830bb435
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-06 13:41:01 -0400

    Division Test

---
+++ division_test.rb
@@ -0,0 +1,25 @@
+
+# def denominator
+# 	denominator != 0
+# end
+
+require_relative "division.rb"
+
+# Prompts the user to give a value to the numerator and the denominator
+# and then divides the numerator by the denominatr to return the quotient.
+
+# def quotient(numerator,denominator)
+#  	if denominator == 0
+# 		puts "Does Not Exist"
+# 	else 
+# 		puts "#{numerator} / #{denominator} = #{numerator / denominator}"
+# 	end
+# end
+
+# puts "Numerator: " 
+# numerator = gets.to_i
+ 
+# puts "Denominator: " 
+# denominator = gets.to_i 
+ 
+# quotient(numerator,denominator) 
